1b65bc5c8086d0490bbaa8e5fcf23d4799bd0ae0
Aleksandr Spitcyn <35581198+Ireesh@users.noreply.github.com>
2021-01-28 23:36:49 +0300

    Delete SpringHomework1 directory

--- SpringHomework1/src/main/java/com/geekbrains/homework_spring_1/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.geekbrains.homework_spring_1;
-
-import java.util.List;
-
-public interface Repository {
-    List<Product> getAllProducts();
-    Product getProductById(int id);
-    void addProduct(Product product);
-}
