554a6b72a723d86ea2bf69f452671bc4bbf1ab83
Ireesh <ireesh45@gmail.com>
2021-02-18 03:18:19 +0300

    - fix problem with client side validation

--- InstaBot/src/main/resources/static/js/src/login.js
+++ InstaBot/src/main/resources/static/js/src/login.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-    var pattern = new RegExp('[a-zA-Z0-9!-_][^()\'`]{8,}$');
+    var patternPass = new RegExp('[a-zA-Z0-9!-_][^()\'`]{8,}$');
+    var patternEmail = new RegExp('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$');
     $('#password, #passwordConfirm').on('keyup', function() {
-        if ($('#password').val() != '' && $('#passwordConfirm').val() != '' && $('#password').val() == $('#passwordConfirm').val() && pattern.test($('#password').val())) {
-            $('.pwds').removeClass('is-invalid');
+        if ($('#password').val() != '' && $('#passwordConfirm').val() != '' && $('#password').val() == $('#passwordConfirm').val()) {
+            $($('#passwordConfirm')).removeClass('is-invalid');
         } else {
-            $('.pwds').addClass('is-invalid');
+            $($('#passwordConfirm')).addClass('is-invalid');
         }
     });
 
@@ -17,4 +18,19 @@ $(document).ready(function(){
               }
               currForm1.classList.add('was-validated');
     }, false);
+
+    currForm.querySelectorAll('.form-control').forEach(input => {
+        input.addEventListener(('input'), () => {
+            if (patternPass.test($('#password').val())) {
+                $('#password').removeClass('is-invalid');
+            } else {
+                $('#password').addClass('is-invalid');
+            }
+             if (patternEmail.test($('#email').val())) {
+                $('#email').removeClass('is-invalid');
+             } else {
+                $('#email').addClass('is-invalid');
+             }
+        });
+    });
 });
